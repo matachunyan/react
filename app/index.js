bf19b5ce33610dde48e0ec2e7773a7eb526f12e7
matachunyan <qyc900329@sina.com>
2017-06-09 02:17:34 +0800

    引入jQuery

--- app/index.js
+++ app/index.js
@@ -1,11 +1,16 @@
-import _ from 'lodash';
+import _ from 'lodash'
+import j from 'jquery'
+
 function component () {
-    var element = document.createElement('div');
+
+     var element = j('<div></div>');
 
     /* lodash is required for the next line to work */
-    element.innerHTML = _.join(['Hello','webpack'], ' ');
 
-    return element;
+      element.html(_.join(['how are you','webpack'], ' '))
+
+
+      return element.get(0);
 }
 
 document.body.appendChild(component());
